e65eb694196bbd4f49df65c068fa93fc4235ad35
Shabuddin Khan <shabuddink@avyanconsulting.com>
2018-03-20 14:26:46 +0530

    updated deployment scripts

--- deploy-azuresecurityscenarios.ps1
+++ deploy-azuresecurityscenarios.ps1
@@ -22,7 +22,9 @@
 #>
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [ValidateScript({
         if ( (Get-Content -Path $PSScriptRoot\azure-security-poc.json | ConvertFrom-Json).PSObject.Properties.Name -contains "$_") {
             $true
@@ -34,124 +36,112 @@ param (
     [string]
     $Scenario,
 
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [ValidateSet("Deploy","Attack","Remediate")] 
     [string]
     $Command = "Deploy",
 
     # Enter Subscription Id for deployment.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [Alias("subscription")]
     [guid]
     $SubscriptionId,
 
     # Enter AAD Username with Owner permission at subscription level and Global Administrator at AAD level.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [Alias("user")]
     [string]
     $UserName,
 
     # Enter AAD Username password as securestring.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [Alias("pwd")]
     [securestring]
     $Password,
 
     # Enter AAD Username password as securestring.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Deployment"
+    )]
     [string]
-    $Location = "East US"
+    $Location = "East US",
+
+    # use this switch for help information.
+    [Parameter(Mandatory = $false,
+        ParameterSetName = "Help"
+    )]
+    [switch]
+    $Help 
 )
-begin {
-    $ErrorActionPreference = 'Stop'
-    $moduleFolderPath = "$PSScriptRoot\common\modules\powershell\asc.poc.psd1"
-    Import-Module $moduleFolderPath
-    $scenarios = Get-Content -Path $PSScriptRoot\azure-security-poc.json | ConvertFrom-Json
-    $storageContainerName = "artifacts"
-    $artifactsResourceGroupName = 'azuresecuritypoc-artifacts-' + (Get-StringHash $subscriptionId).substring(0,5) + '-rg'
-    $deploymentHash = (Get-StringHash $artifactsResourceGroupName).substring(0,10)
-    $storageAccountName = 'stage' + $deploymentHash
-    $artifactStagingDirectories = @(
-        "$PSScriptRoot\common"
-        "$PSScriptRoot\resources"
-    )
-    if((Get-AzureRmContext).Subscription -eq $null){
-        if ($SubscriptionId -eq $null -or $UserName -eq $null -or $Password -eq $null) {
-            throw "Kindly make sure SubscriptionID, Username and Password parameters are provided during the deployment."
-        }
-        ### Create the credential object
-        $credential = New-Object System.Management.Automation.PSCredential($UserName, $Password)
-        try {
-            Write-Verbose "Setting AzureRM context to Subscription Id - $SubscriptionId."
-            Set-AzureRmContext -Subscription $SubscriptionId
-        }
-        catch {
-            Write-Verbose "Login to Subscription - $SubscriptionId"
-            Login-AzureRmAccount -Subscription $SubscriptionId -Credential $credential
-        }
+
+$ErrorActionPreference = 'Stop'
+$scenarios = Get-Content -Path $PSScriptRoot\azure-security-poc.json | ConvertFrom-Json
+if ($Help) {
+    $scenarios.PSObject.Properties | Select-Object -Property Name ,@{Name="Description"; Expression = {$_.value.description}} | Format-Table
+    Break
+}
+$moduleFolderPath = "$PSScriptRoot\common\modules\powershell\asc.poc.psd1"
+Import-Module $moduleFolderPath
+$storageContainerName = "artifacts"
+$artifactsResourceGroupName = 'azuresecuritypoc-artifacts-' + (Get-StringHash $subscriptionId).substring(0,5) + '-rg'
+$deploymentHash = (Get-StringHash $artifactsResourceGroupName).substring(0,10)
+$storageAccountName = 'stage' + $deploymentHash
+$artifactStagingDirectories = @(
+    "$PSScriptRoot\common"
+    "$PSScriptRoot\resources"
+)
+if((Get-AzureRmContext).Subscription -eq $null){
+    if ($SubscriptionId -eq $null -or $UserName -eq $null -or $Password -eq $null) {
+        throw "Kindly make sure SubscriptionID, Username and Password parameters are provided during the deployment."
     }
-
-    # Create Resourcegroup
-    New-AzureRmResourceGroup -Name $artifactsResourceGroupName -Location $Location -Force
-
-    Write-Verbose "Check if artifacts storage account exists."
-    $storageAccount = (Get-AzureRmStorageAccount | Where-Object {$_.StorageAccountName -eq $storageAccountName})
-
-    # Create the storage account if it doesn't already exist
-    if ($storageAccount -eq $null) {
-        Write-Verbose "Artifacts storage account does not exists."
-        Write-Verbose "Provisioning artifacts storage account."
-        $storageAccount = New-AzureRmStorageAccount -StorageAccountName $storageAccountName -Type 'Standard_LRS' `
-            -ResourceGroupName $artifactsResourceGroupName -Location $Location
-        Write-Verbose "Artifacts storage account provisioned."
-        Write-Verbose "Creating storage container to upload a blobs."
-        New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
+    ### Create the credential object
+    $credential = New-Object System.Management.Automation.PSCredential($UserName, $Password)
+    try {
+        Write-Verbose "Setting AzureRM context to Subscription Id - $SubscriptionId."
+        Set-AzureRmContext -Subscription $SubscriptionId
     }
-    else {
-        New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
+    catch {
+        Write-Verbose "Login to Subscription - $SubscriptionId"
+        Login-AzureRmAccount -Subscription $SubscriptionId -Credential $credential
     }
+}
+<#
+# Create Resourcegroup
+New-AzureRmResourceGroup -Name $artifactsResourceGroupName -Location $Location -Force
 
-    # Copy files from the local storage staging location to the storage account container
-    foreach ($artifactStagingDirectory in $artifactStagingDirectories) {
-        $ArtifactFilePaths = Get-ChildItem $ArtifactStagingDirectory -Recurse -File | ForEach-Object -Process {$_.FullName}
-        foreach ($SourcePath in $ArtifactFilePaths) {
-            Set-AzureStorageBlobContent -File $SourcePath -Blob $SourcePath.Substring((Split-Path($ArtifactStagingDirectory)).length + 1) `
-                -Container $storageContainerName -Context $storageAccount.Context -Force
-        }
-    }
+Write-Verbose "Check if artifacts storage account exists."
+$storageAccount = (Get-AzureRmStorageAccount | Where-Object {$_.StorageAccountName -eq $storageAccountName})
 
+# Create the storage account if it doesn't already exist
+if ($storageAccount -eq $null) {
+    Write-Verbose "Artifacts storage account does not exists."
+    Write-Verbose "Provisioning artifacts storage account."
+    $storageAccount = New-AzureRmStorageAccount -StorageAccountName $storageAccountName -Type 'Standard_LRS' `
+        -ResourceGroupName $artifactsResourceGroupName -Location $Location
+    Write-Verbose "Artifacts storage account provisioned."
+    Write-Verbose "Creating storage container to upload a blobs."
+    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
+}
+else {
+    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
 }
 
-process {
-
-    $userInput = $null
-    do {
-        Write-Host ""
-        $userInput = Read-Host -Prompt "Enter scenario name to deploy. Type 'Help' to get the list of scenarios. Type 'Exit' to exit."
-        switch ($userInput) {
-            'Help' {
-                $scenarios.PSObject.Properties | Select-Object -Property Name ,@{Name="Description"; Expression = {$_.value.description}} | Format-Table
-            }
-            {($scenarios).PSObject.Properties.Name -contains "$_"} {
-                $scenario = $_
-                $caseNo = ($scenarios | Select-Object -expandproperty $_).caseNo
-                if ($SubscriptionId -eq $null) {
-                    & "$PSScriptRoot\scenarios\$scenario\deploy.ps1" -CaseNo $caseNo -artifactsStorageAccountName $storageAccountName -Verbose
-                }
-                else {
-                    & "$PSScriptRoot\scenarios\$scenario\deploy.ps1" -CaseNo $caseNo -SubscriptionId $SubscriptionId -UserName $UserName -Password $Password -artifactsStorageAccountName $storageAccountName -Verbose
-                }
-            }
-            'Exit' {
-                Write-Host "Exiting.."
-                Exit
-            }
-            Default {Write-Host "Invalid input.Please enter valid POC number." -ForegroundColor Red}
-        }
-        
-    } until ($userInput -eq 'Exit')
-
+# Copy files from the local storage staging location to the storage account container
+foreach ($artifactStagingDirectory in $artifactStagingDirectories) {
+    $ArtifactFilePaths = Get-ChildItem $ArtifactStagingDirectory -Recurse -File | ForEach-Object -Process {$_.FullName}
+    foreach ($SourcePath in $ArtifactFilePaths) {
+        Set-AzureStorageBlobContent -File $SourcePath -Blob $SourcePath.Substring((Split-Path($ArtifactStagingDirectory)).length + 1) `
+            -Container $storageContainerName -Context $storageAccount.Context -Force
+    }
 }
-
-end {
-}
+#>
+$caseNo = $caseNo = ($scenarios | Select-Object -expandproperty $Scenario).caseNo
+& "$PSScriptRoot\scenarios\$Scenario\deploy.ps1" -CaseNo $caseNo -artifactsStorageAccountName $storageAccountName -Verbose
