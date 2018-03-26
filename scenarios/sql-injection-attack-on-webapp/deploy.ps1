a28d3949a6f4248d9c54dc1a9c7423891988ce29
Shabuddin Khan <shabuddink@avyanconsulting.com>
2018-03-26 18:33:56 +0530

    add code for encrypting SQL.

--- scenarios/sql-injection-attack-on-webapp/deploy.ps1
+++ scenarios/sql-injection-attack-on-webapp/deploy.ps1
@@ -6,19 +6,19 @@ param (
     $Prefix,
 
     # Enter Subscription Id for deployment.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $true)]
     [Alias("subscription")]
     [guid]
     $SubscriptionId,
 
     # Enter AAD Username with Owner permission at subscription level and Global Administrator at AAD level.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $true)]
     [Alias("user")]
     [string]
     $UserName,
 
     # Enter AAD Username password as securestring.
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory = $true)]
     [Alias("pwd")]
     [securestring]
     $Password,
@@ -41,12 +41,9 @@ $deploymentName = "sql-injection-attack-on-webapp"
 $sessionGuid = New-Guid
 $timeStamp = Date -Format dd_yyyy_hh_mm_ss
 $rootFolder = Split-Path(Split-Path($PSScriptRoot))
+Write-Verbose "Initialising transcript."
+Start-Transcript -Path "$rootFolder\logs\transcript_$timeStamp.txt" -Append -Force
 $moduleFolderPath = "$rootFolder\common\modules\powershell\asc.poc.psd1"
-$artifactStagingDirectories = @(
-    #"$rootFolder\common"
-    #"$rootFolder\resources"
-    "$PSScriptRoot"
-)
 $workloadResourceGroupName = "{0}-{1}" -f $Prefix, $deploymentName
 $commonTemplateParameters = New-Object -TypeName Hashtable # Will be used to pass common parameters to the template.
 $artifactsLocation = '_artifactsLocation'
@@ -59,9 +56,16 @@ $aadAppIdentifierUris = $aadAppHomepage
 $deploymentPassword = $parametersObj.parameters.commonReference.value.deploymentPassword
 $secureDeploymentPassword = $deploymentPassword | ConvertTo-SecureString -AsPlainText -Force
 $tenantId = (Get-AzureRmContext).Tenant.TenantId
-if ($tenantId -eq $null){$tenantId = (Get-AzureRmContext).Tenant.Id}
-
-if((Get-AzureRmContext).Subscription -eq $null){
+if ($tenantId -eq $null) {$tenantId = (Get-AzureRmContext).Tenant.Id}
+$clientIPAddress = Invoke-RestMethod http://ipinfo.io/json | Select-Object -exp ip
+$clientIPHash = (Get-StringHash $clientIPAddress).substring(0, 5)
+$databaseName = $parametersObj.parameters.workload.value.sqlServer.databases[0].name
+$cmkName = "CMK1" 
+$cekName = "CEK1" 
+$keyName = "CMK1"
+$artifactsStorageAccKeyType = "StorageAccessKey"
+$sqlsmodll = (Get-ChildItem "$env:programfiles\WindowsPowerShell\Modules\SqlServer" -Recurse -File -Filter "Microsoft.SqlServer.Smo.dll").FullName
+if ((Get-AzureRmContext).Subscription -eq $null) {
     if ($SubscriptionId -eq $null -or $UserName -eq $null -or $Password -eq $null) {
         throw "Kindly make sure SubscriptionID, Username and Password parameters are provided during the deployment."
     }
@@ -77,15 +81,31 @@ if((Get-AzureRmContext).Subscription -eq $null){
         Login-AzureRmAccount -Subscription $SubscriptionId -Credential $credential
     }
 }
+$userPrincipalName = (Get-AzureRmContext).Account.Id
 
-Write-Verbose "Initialising transcript."
-Start-Transcript -Path "$rootFolder\logs\transcript_$timeStamp.txt" -Append -Force
+Connect-AzureAD -TenantId $tenantId -Credential $credential
+
+Write-Verbose -Message "Create Azure AD application in Default directory"
+if ((Get-AzureADApplication -SearchString $aadAppDisplayName) -eq $null) {
+    $aadApplication = New-AzureADApplication -DisplayName $aadAppDisplayName -IdentifierUris $aadAppIdentifierUris
+    $aadApplicationClientId = $aadApplication.AppId
+    $aadApplicationObjectId = $aadApplication.ObjectId
+    New-AzureADApplicationPasswordCredential -ObjectId $aadApplicationObjectId -Value $deploymentPassword
+    New-AzureADServicePrincipal -AppId $aadApplicationClientId
+    Write-Verbose -Message "Azure Active Directory apps creation successful. AppID is $aadApplicationClientId"
+}
+else {
+    $aadApplication = Get-AzureADApplication -SearchString $aadAppDisplayName
+    $aadApplicationClientId = $aadApplication.AppId
+    $aadApplicationObjectId = $aadApplication.ObjectId
+    New-AzureADApplicationPasswordCredential -ObjectId $aadApplicationObjectId -Value $deploymentPassword
+}
 
 Write-Verbose "Importing custom modules."
 Import-Module $moduleFolderPath
 Write-Verbose "Module imported."
 
-# Register RPs
+<# Register RPs
 $resourceProviders = @(
     "Microsoft.Storage",
     "Microsoft.Compute",
@@ -100,7 +120,7 @@ if($resourceProviders.length) {
     }
 }
 #>
-$deploymentHash = (Get-StringHash $workloadResourceGroupName).substring(0,10)
+$deploymentHash = (Get-StringHash $workloadResourceGroupName).substring(0, 10)
 if ($artifactsStorageAccountName -eq $null) {
     $storageAccountName = 'stage' + $deploymentHash
 }
@@ -121,17 +141,28 @@ $storageAccount = (Get-AzureRmStorageAccount | Where-Object {$_.StorageAccountNa
 
 # Create the storage account if it doesn't already exist
 if ($storageAccount -eq $null) {
+    $artifactStagingDirectories = @(
+        "$rootFolder\common"
+        "$rootFolder\resources"
+        "$PSScriptRoot"
+    )
     Write-Verbose "Artifacts storage account does not exists."
     Write-Verbose "Provisioning artifacts storage account."
     $storageAccount = New-AzureRmStorageAccount -StorageAccountName $storageAccountName -Type 'Standard_LRS' `
         -ResourceGroupName $workloadResourceGroupName -Location $Location
     Write-Verbose "Artifacts storage account provisioned."
     Write-Verbose "Creating storage container to upload a blobs."
-    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
+    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue
 }
 else {
-    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue *>&1
+    $artifactStagingDirectories = @(
+        "$PSScriptRoot"
+    )
+    New-AzureStorageContainer -Name $storageContainerName -Context $storageAccount.Context -ErrorAction SilentlyContinue
 }
+
+# Retrieve Access Key 
+$artifactsStorageAccKey = (Get-AzureRmResource | Where-Object ResourceName -eq $storageAccountName | Get-AzureRmStorageAccountKey)[0].value 
 
 # Copy files from the local storage staging location to the storage account container
 foreach ($artifactStagingDirectory in $artifactStagingDirectories) {
@@ -143,10 +174,12 @@ foreach ($artifactStagingDirectory in $artifactStagingDirectories) {
 }
 
 # Generate the value for artifacts location & 4 hour SAS token for the artifacts location.
-$commonTemplateParameters[$artifactsLocation] = $storageAccount.Context.BlobEndPoint + $storageContainerName
-$commonTemplateParameters[$artifactsLocationSasToken] = New-AzureStorageContainerSASToken -Container $storageContainerName -Context $storageAccount.Context -Permission r -ExpiryTime (Get-Date).AddHours(4)
+$artifactsLocation = $storageAccount.Context.BlobEndPoint + $storageContainerName
+$commonTemplateParameters['_artifactsLocation'] = $artifactsLocation
+$artifactsLocationSasToken = New-AzureStorageContainerSASToken -Container $storageContainerName -Context $storageAccount.Context -Permission r -ExpiryTime (Get-Date).AddHours(4)
+$commonTemplateParameters['_artifactsLocationSasToken'] = $artifactsLocationSasToken
 
-########### Create Azure Active Directory apps in default directory ###########
+<########### Create Azure Active Directory apps in default directory ###########
 Write-Verbose -Message "Create Azure AD application in Default directory"
 
 if ((Get-AzureRmADApplication -IdentifierUri $aadAppIdentifierUris) -eq $null) {
@@ -160,11 +193,11 @@ else {
     $aadApplicationClientId = $aadApplication.ApplicationId.Guid
     $aadApplicationObjectId = $aadApplication.ObjectId.Guid
 }
-
+#>
 # Update parameter file with deployment values.
 Write-Verbose "Updating parameter file."
-$parametersObj.parameters.commonReference.value._artifactsLocation = $commonTemplateParameters[$artifactsLocation]
-$parametersObj.parameters.commonReference.value._artifactsLocationSasToken = $commonTemplateParameters[$artifactsLocationSasToken]
+$parametersObj.parameters.commonReference.value._artifactsLocation = $commonTemplateParameters['_artifactsLocation']
+$parametersObj.parameters.commonReference.value._artifactsLocationSasToken = $commonTemplateParameters['_artifactsLocationSasToken']
 $parametersObj.parameters.commonReference.value.prefix = $Prefix
 $parametersObj.parameters.workload.value.keyVault.accessPolicies[0].applicationId = $aadApplicationClientId
 $parametersObj.parameters.workload.value.keyVault.accessPolicies[0].objectId = $aadApplicationObjectId
@@ -173,3 +206,81 @@ $parametersObj.parameters.workload.value.keyVault.accessPolicies[0].tenantId = $
 
 Write-Verbose "Initiate Deployment for TestCase - $Prefix"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $workloadResourceGroupName -TemplateFile "$PSScriptRoot\templates\workload\azuredeploy.json" -TemplateParameterFile $tmp -Name $armDeploymentName -Mode Incremental -DeploymentDebugLogLevel All -Verbose -Force
+
+# Updating SQL server firewall rule
+Write-Verbose -Message "Updating SQL server firewall rule."
+$allResource = (Get-AzureRmResource | Where-Object ResourceGroupName -EQ $workloadResourceGroupName)
+$sqlServerName = ($allResource | Where-Object ResourceType -eq 'Microsoft.Sql/servers').ResourceName
+
+New-AzureRmSqlServerFirewallRule -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -FirewallRuleName "ClientIpRule$clientIPHash" -StartIpAddress $clientIPAddress -EndIpAddress $clientIPAddress -ErrorAction SilentlyContinue
+New-AzureRmSqlServerFirewallRule -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -FirewallRuleName "AllowAzureServices" -StartIpAddress 0.0.0.0 -EndIpAddress 0.0.0.0 -ErrorAction SilentlyContinue
+
+Start-Sleep -Seconds 15
+
+# Import SQL bacpac and update azure SQL DB Data masking policy
+Write-Verbose -Message "Importing SQL bacpac and Updating Azure SQL DB Data Masking Policy"
+
+# Importing bacpac file
+Write-Verbose -Message "Importing SQL backpac from release artifacts storage account."
+$sqlBacpacUri = "$artifactsLocation/$deploymentName/artifacts/ContosoPayments.bacpac"
+try {
+    New-AzureRmSqlDatabaseImport -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -DatabaseName $databaseName -StorageKeytype $artifactsStorageAccKeyType -StorageKey $artifactsStorageAccKey -StorageUri "$sqlBacpacUri" -AdministratorLogin 'sqlAdmin' -AdministratorLoginPassword $secureDeploymentPassword -Edition Standard -ServiceObjectiveName S0 -DatabaseMaxSizeBytes 50000
+}
+catch {
+    $_
+}
+
+Start-Sleep -Seconds 100
+
+Write-Verbose -Message "Updating Azure SQL DB Data masking policy on FirstName & LastName Column."
+Set-AzureRmSqlDatabaseDataMaskingPolicy -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -DatabaseName $databaseName -DataMaskingState Enabled
+Start-Sleep -s 60
+New-AzureRmSqlDatabaseDataMaskingRule -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -DatabaseName $databaseName -SchemaName "dbo" -TableName "Customers" -ColumnName "FirstName" -MaskingFunction Default
+New-AzureRmSqlDatabaseDataMaskingRule -ResourceGroupName $workloadResourceGroupName -ServerName $sqlServerName -DatabaseName $databaseName -SchemaName "dbo" -TableName "Customers" -ColumnName "LastName" -MaskingFunction Default
+
+# Encrypting Credit card information within database
+try {
+    Write-Host ("`nStep 12: Encrypt SQL DB column Credit card Information" ) -ForegroundColor Green
+    # Connect to your database.
+    Add-Type -Path $sqlsmodll
+    Write-Host -ForegroundColor Yellow "`t* Connecting database - $databaseName on $sqlServerName"
+    $connStr = "Server=tcp:" + $sqlServerName + ".database.windows.net,1433;Initial Catalog=" + "`"" + $databaseName + "`"" + ";Persist Security Info=False;User ID=" + "`"" + "sqladmin" + "`"" + ";Password=`"" + "$deploymentPassword" + "`"" + ";MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    $connection = New-Object Microsoft.SqlServer.Management.Common.ServerConnection
+    $connection.ConnectionString = $connStr
+    $connection.Connect()
+    $server = New-Object Microsoft.SqlServer.Management.Smo.Server($connection)
+    $database = $server.Databases[$databaseName]
+
+    #Granting Users & ServicePrincipal full access on Keyvault
+    Write-Host ("`t* Giving Key Vault access permissions to the Users and ServicePrincipal ..") -ForegroundColor Yellow
+    $KeyVaultName = ($allResource | Where-Object ResourceType -eq 'Microsoft.KeyVault/vaults').ResourceName
+    Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -UserPrincipalName $userPrincipalName -ResourceGroupName $workloadResourceGroupName -PermissionsToKeys all -PermissionsToSecrets all
+    Write-Host ("`t* Granted permissions to the users and serviceprincipals ..") -ForegroundColor Yellow
+    Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ServicePrincipalName $aadApplicationClientId -ResourceGroupName $workloadResourceGroupName -PermissionsToKeys all -PermissionsToSecrets all
+
+    # Creating KeyVault Key to encrypt DB
+    Write-Host -ForegroundColor Yellow "`t* Creating a New Keyvault key."
+    $key = (Add-AzureKeyVaultKey -VaultName $KeyVaultName -Name $keyName -Destination 'Software').ID
+
+    # Switching SQL commands context to the AD Application
+    Write-Host -ForegroundColor Yellow "`t* Creating SQL Column Master Key & Column Encryption Key."
+    $cmkSettings = New-SqlAzureKeyVaultColumnMasterKeySettings -KeyURL $key
+    $sqlMasterKey = Get-SqlColumnMasterKey -Name $cmkName -InputObject $database -ErrorAction SilentlyContinue
+    if ($sqlMasterKey) {Write-Host -ForegroundColor Yellow "`t* SQL Master Key $cmkName already exists."} 
+    Else {New-SqlColumnMasterKey -Name $cmkName -InputObject $database -ColumnMasterKeySettings $cmkSettings}
+    Add-SqlAzureAuthenticationContext -ClientID $aadApplicationClientId -Secret $deploymentPassword -Tenant $tenantID
+    New-SqlColumnEncryptionKey -Name $cekName -InputObject $database -ColumnMasterKey $cmkName
+
+    Write-Host -ForegroundColor Yellow "`t* SQL encryption has been successfully created. Encrypting SQL Columns.."
+    # Encrypt the selected columns (or re-encrypt, if they are already encrypted using keys/encrypt types, different than the specified keys/types.
+    $ces = @()
+    $ces += New-SqlColumnEncryptionSettings -ColumnName "dbo.Customers.CreditCard_Number" -EncryptionType "Deterministic" -EncryptionKey $cekName
+    $ces += New-SqlColumnEncryptionSettings -ColumnName "dbo.Customers.CreditCard_Code" -EncryptionType "Deterministic" -EncryptionKey $cekName
+    $ces += New-SqlColumnEncryptionSettings -ColumnName "dbo.Customers.CreditCard_Expiration" -EncryptionType "Deterministic" -EncryptionKey $cekName
+    Set-SqlColumnEncryption -InputObject $database -ColumnEncryptionSettings $ces
+    Write-Host -ForegroundColor Yellow "`t* Column CreditCard_Number, CreditCard_Code, CreditCard_Expiration have been successfully encrypted"            
+}
+catch {
+    Write-Host -ForegroundColor Red "`t Column encryption has failed."
+    throw $_
+}
