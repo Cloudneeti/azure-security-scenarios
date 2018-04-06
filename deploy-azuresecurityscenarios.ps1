59a34b58f31ae6fc1c1a993018ffe27734f4452b
Shabuddin Khan <shabuddink@avyanconsulting.com>
2018-04-06 17:54:37 +0530

    updated license text to all ps1 files.

--- deploy-azuresecurityscenarios.ps1
+++ deploy-azuresecurityscenarios.ps1
@@ -2,11 +2,27 @@
 .SYNOPSIS
     Short description
 .DESCRIPTION
-    Long description
+
+Copyright (c) Avyan Consulting Corp. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND ONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 .EXAMPLE
-    Example of how to use this cmdlet
+    .\deploy-azuresecurityscenarios.ps1 -Help
+
+    Run this command to get list of supported scenarios
+
 .EXAMPLE
-    Another example of how to use this cmdlet
+    .\deploy-azuresecurityscenarios.ps1 -Scenario "virus-attack-on-vm" -Command Deploy -Verbose
+
+    If you are using Cloud Shell you can simply pass 2 parameters to run the deployment.
+
+.EXAMPLE
+    .\deploy-azuresecurityscenarios.ps1 -SubscriptionId <subscriptionId> -UserName <username> -Password <securePassword> -Scenario <scenario> -Command Deploy -Verbose
+
+    If you are running on a local machine pass additional parameters to connect to subscription and run the deployment.
+
 .INPUTS
     Inputs to this cmdlet (if any)
 .OUTPUTS
@@ -20,6 +36,7 @@
 .FUNCTIONALITY
     The functionality that best describes this cmdlet
 #>
+
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $false,
