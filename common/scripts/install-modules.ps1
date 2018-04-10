061eb8505d5d924227e78209dc6ffe82736b91f3
Himalay Kondekar <kondekarhimalay@gmail.com>
2018-04-10 14:55:10 +0530

    added script for install module

---
+++ common/scripts/install-modules.ps1
@@ -0,0 +1,37 @@
+[CmdletBinding()]
+param
+(
+    
+)
+
+# Manage Session Configuration
+Set-StrictMode -Version Latest
+
+$rootFolder = Split-Path(Split-Path(Split-Path $MyInvocation.MyCommand.Path)) # this variable refers to Cloudneeti.Deployment folder path
+
+## Required modules 
+$requiredModules = @{
+    'AzureRM' = '5.2.0';
+    'AzureAD' = '2.0.0.131'
+}
+
+# Check and Install required modules
+Install-RequiredModules -moduleNames $requiredModules
+Start-Sleep 30
+
+# Import required modules
+$modules = $requiredModules.Keys
+try {
+    Write-Host "Importing required modules."
+    foreach ($module in $modules) {
+        $moduleImportStatus = Get-Module -Name $module
+        if ($moduleImportStatus -eq $null) {
+            Write-Host "Importing - $module."
+            Import-Module -Name $module -RequiredVersion $requiredModules[$module]
+            Write-Host "Module - $module imported."
+        }
+    }
+}
+catch {
+    Throw $_
+}
